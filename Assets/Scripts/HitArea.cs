b1a718e138656e21ad840c58d7e8b8ed27e8c40a
JOSHUA-3403\joshua <joshua3403@gmail.com>
2018-02-16 11:57:29 -0400

    Onlinegame

---
+++ Assets/Scripts/HitArea.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitArea : MonoBehaviour {
+
+	void Damage(AttackArea.AttackInfo attackInfo){
+				transform.root.SendMessage ("Damage", attackInfo);
+		}
+	}
---
+++ Assets/Scripts/SearchArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SearchArea : MonoBehaviour {
+	EnemyCtrl enemyCtrl;
+
+	// Use this for initialization
+	void Start () {
+		// EnemyCtrl을 미리 준비한다.
+		enemyCtrl = transform.root.GetComponent<EnemyCtrl> ();
+	}
+	
+	void OnTriggerStay(Collider other)
+	{
+		// Player태그를 타깃으로 한다.
+		if (other.tag == "Player")
+						enemyCtrl.SetAttackTarget (other.transform);
+	}
+}
